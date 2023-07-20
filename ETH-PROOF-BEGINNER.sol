81556b01fbecc2f46e62c1f999e646e360719d08
UTKARSH PANDEY <92950393+Utkarsh-Pandey-GitHub@users.noreply.github.com>
2023-07-20 17:44:09 +0530

    Update ETH-PROOF-BEGINNER.sol

--- ETH-PROOF-BEGINNER.sol
+++ ETH-PROOF-BEGINNER.sol
@@ -4,21 +4,21 @@ pragma solidity 0.8.18;
 
 contract MyToken {
 
-    // public variables here
+    
     string public tokenName = "META";
     string public tokenAbbrv = "MTA";
     uint256 public totalSupply = 0;
 
-    // mapping variable here
+    
     mapping (address=>uint) public balances;
 
-    // mint function
+    
     function mint(address _address,uint _value)public{
         totalSupply += _value;
         balances[_address] += _value;
     }
 
-    // burn function
+    
     function burn (address _address,uint _value)public {
         if (balances[_address]>=_value) {
             
